82782de58acc413c2c70eaa01502822ff6d5a6ef
Nouzan <linouzan@gmail.com>
2018-11-16 10:31:32 +0800

    finish index && upload

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from bottle import route, run, template
+from bottle import route, run, template, request, redirect
+import os
 
 @route('/')
 def hello():
     return template('index')
 
+@route('/upload', method='GET')
+def upload():
+    return template('upload')
+
+@route('/upload', method='POST')
+def do_upload():
+    upload     = request.files.get('upload')
+    name, ext = os.path.splitext(upload.filename)
+    if ext not in ('.png','.jpg','.jpeg'):
+        return 'File extension not allowed.'
+
+    save_path = 'media/uploads/'
+    upload.save(save_path) # appends upload.filename automatically
+    return """
+    <p>上传成功！</p>
+    <a href="/">返回主页</a>
+    """
+
 run(host='0.0.0.0', port=8001, debug=True)
+
